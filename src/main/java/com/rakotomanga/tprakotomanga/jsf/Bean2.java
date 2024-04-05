210776de0d759b3644f794bedf88ddc2dab51e5d
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-04-05 16:50:30 +0300

    Etat final tp 2

---
+++ src/main/java/com/rakotomanga/tprakotomanga/jsf/Bean2.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.rakotomanga.tprakotomanga.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Hasina
+ */
+@Named(value = "bean2")
+@RequestScoped
+public class Bean2 {
+
+    /**
+     * Creates a new instance of Bean2
+     */
+    public Bean2() {
+    }
+    
+    private int nombre;
+
+    public String afficher() {
+        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+    
+}
