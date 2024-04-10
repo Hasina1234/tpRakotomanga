c2d09538d1f161cc4605b86c4d49f48e237a58e2
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2024-04-10 20:03:59 +0300

    TP 3 - Templates pour JSF - Etat final

--- src/main/java/com/rakotomanga/tprakotomanga/jsf/Bean2.java
+++ src/main/java/com/rakotomanga/tprakotomanga/jsf/Bean2.java
@@ -26,7 +26,7 @@ public class Bean2 {
     private int nombre;
 
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
     
     public int getNombre() {
